f56a4830edc4bdab9c34c4acea42401a060182a1
Mustakim Ali <mustakimalikiash@gmail.com>
2025-11-17 11:20:15 +0600

    Update odd.php

--- MID/Lab_Task_2/odd.php
+++ MID/Lab_Task_2/odd.php
@@ -10,6 +10,21 @@
 <body>
 <h2>Student Registration </h2>
 
+<form> 
+
+Full Name:<br>
+<input type="text"><br><br>
+
+Email:<br>
+<input type="email"> <br><br>
+
+Password:<br>
+<input type="password"> <br><br>
+
+Confirm Password:<br>
+<input type="password"> <br><br>
+
+</form>
 </body>
 
 </html>
