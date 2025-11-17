2aa0a44c63ee27515a4bdf83b6d3737b7a487663
Mustakim Ali <mustakimalikiash@gmail.com>
2025-11-17 11:44:02 +0600

    Update odd.php

--- MID/Lab_Task_2/odd.php
+++ MID/Lab_Task_2/odd.php
@@ -5,13 +5,50 @@
 <head>
      <title>Student Registration Form </title>
 
+<style>
+
+    body{
+        background-color: #f2f2f2;
+        font-family: Arial, sans-serif;
+        margin: 20px;
+    }
+
+    h2{
+        color: #333333;
+    }
+
+    form{
+        
+        background-color: #ffffff;
+        padding: 20px;
+        border-radius: 5px;;
+        margin-bottom: 30px;
+    }
+
+
+
+       
+    fieldset {
+      background: #fff;
+      border-radius: 11px;
+      border: 1px solid #ccc;
+      box-shadow: 0 2px 9px #e3e3ed;
+      width: 440px;
+      margin-bottom: 22px;
+      padding: 25px 30px;
+    }
+
+</style>
+
 </head>
 
 <body>
-<h2>Student Registration </h2>
+    
+
 
 <form> 
-
+    <h2>Student Registration </h2>
+<fieldset>
 Full Name:<br>
 <input type="text"><br>
 
@@ -25,13 +62,18 @@ Confirm Password:<br>
 <input type="password"> <br>
 
 <button type="submit"> Register </button>
-
+</fieldset>
 </form>
 
+<form>
+
+<fieldset>
 <h2>Course Registration </h2>
 Course name:<br>
 <input type="text"><br>
 <button> Add Course </button>
+</fieldset>
+</form>
 
 </body>
 
